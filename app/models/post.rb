0c0c522bbb1f5048f37c5c58c7af4328f96e978b
Ualisson Rodrigo <ualissontourniquet@hotmail.com>
2021-12-10 12:17:09 -0300

    adding alert to the model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   extend FriendlyId
 
   validates :title, :author, :body, presence: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   friendly_id :title, use: :slugged
 
